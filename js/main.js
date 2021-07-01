0e0b745f8129c41a8cff4e8ea29ce7b078abc5dd
Joseph <joseph14531@gmail.com>
2021-06-30 23:20:45 -0500

    Se ha terminado el proyecto en mobile y se le ha dado animaciones

--- js/main.js
+++ js/main.js
@@ -4,16 +4,20 @@
 let enlacesheader = document.querySelectorAll(".enlaces-header")[0];
 let semaforo = true;  
 
-let btnclose = document.getElementById("btndiv");
+
+
+AOS.init();
+
+/* let btnclose = document.getElementById("btndiv");
 let btnclose2 = document.getElementById("btnburger");
 btnclose.addEventListener("click", addClase);
+ */
 
-
-function addClase(){
-    alert("Hola");
-    /* btnclose2.classList.remove("hamburger"); */
-    document.getElementById("btnburger").classList.remove("hamburger");
-}
+// function addClase(){
+//     alert("Hola");
+//     /* btnclose2.classList.remove("hamburger"); */
+//     document.getElementById("btnburger").classList.remove("hamburger");
+// }
 
 document.querySelectorAll(".hamburger")[0].addEventListener("click", function(){ 
 
@@ -40,3 +44,30 @@ enlacesheader.classList.toggle("menudos");
 console.log("--------->",semaforo);
 
 })
+
+// TODO - SCROLL
+
+let ubcacionprincipal = window.pageYOffset;
+console.log(ubcacionprincipal);
+
+window.addEventListener("scroll", function(){
+    
+    let desplazamientoactual = window.pageYOffset;
+    this.console.log("Desplazamiento---->",desplazamientoactual);
+    this.console.log("Ubicacionprincipal---->", ubcacionprincipal);
+    if(ubcacionprincipal >= desplazamientoactual){
+        console.log("====> en el if :", ubcacionprincipal, desplazamientoactual);
+        this.document.getElementsByTagName("nav")[0].style.top = "0px";
+        console.log("No entro aqui");
+    }else{
+        console.log("====> en el else :", ubcacionprincipal, desplazamientoactual);
+        this.document.getElementsByTagName("nav")[0].style.top = "-100px";
+        
+    }
+
+    ubcacionprincipal = desplazamientoactual;
+    console.log("Paso al final =====>" , ubcacionprincipal);
+
+})
+
+
